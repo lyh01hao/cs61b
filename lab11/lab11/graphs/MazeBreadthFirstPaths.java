4a7148f787dc88424d7f2098fbe694c06c7c40d7
lyh01hao <lyh01hao@gmail.com>
2020-09-26 11:36:17 +0800

    lab10: completed v1.0

--- lab11/lab11/graphs/MazeBreadthFirstPaths.java
+++ lab11/lab11/graphs/MazeBreadthFirstPaths.java
@@ -1,5 +1,5 @@
 package lab11.graphs;
-
+import edu.princeton.cs.algs4.Queue;
 /**
  *  @author Josh Hug
  */
@@ -9,21 +9,56 @@ public class MazeBreadthFirstPaths extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
+    private int s;
+    private int t;
+    private boolean targetFound = false;
+    private Maze maze;
+    private Queue<Integer> queue;
 
     public MazeBreadthFirstPaths(Maze m, int sourceX, int sourceY, int targetX, int targetY) {
         super(m);
         // Add more variables here!
+        maze = m;
+        s = maze.xyTo1D(sourceX, sourceY);
+        t = maze.xyTo1D(targetX, targetY);
+        distTo[s] = 0;
+        edgeTo[s] = s;
+        queue = new Queue<>();
     }
 
     /** Conducts a breadth first search of the maze starting at the source. */
     private void bfs() {
-        // TODO: Your code here. Don't forget to update distTo, edgeTo, and marked, as well as call announce()
+
+        marked[s] = true;
+        announce();
+        queue.enqueue(s);
+
+        while (!queue.isEmpty()) {
+            int currentVertex = queue.dequeue();
+            if (currentVertex == t) {
+                targetFound = true;
+            }
+            if (targetFound) {
+                return;
+            }
+            for (int w : maze.adj(currentVertex)) {
+                if (!marked[w]) {
+                    queue.enqueue(w);
+                    marked[w] = true;
+                    edgeTo[w] = currentVertex;
+                    distTo[w] = distTo[currentVertex] + 1;
+                    announce();
+                }
+            }
+        }
+
+
     }
 
 
     @Override
     public void solve() {
-        // bfs();
+        bfs();
     }
 }
 
--- lab11/lab11/graphs/MazeCycles.java
+++ lab11/lab11/graphs/MazeCycles.java
@@ -1,5 +1,7 @@
 package lab11.graphs;
 
+import edu.princeton.cs.algs4.StdRandom;
+
 /**
  *  @author Josh Hug
  */
@@ -9,6 +11,8 @@ public class MazeCycles extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
+    private int[] cameFrom;
+    private boolean cycleFound = false;
 
     public MazeCycles(Maze m) {
         super(m);
@@ -16,9 +20,44 @@ public class MazeCycles extends MazeExplorer {
 
     @Override
     public void solve() {
-        // TODO: Your code here!
+        cameFrom = new int[maze.V()];
+        int randX = StdRandom.uniform(1, maze.N() + 1);
+        int randY = StdRandom.uniform(1, maze.N() + 1);
+        int start = maze.xyTo1D(randX, randY);
+        marked[start] = true;
+        cameFrom[start] = start;
+        dfs(start);
+        announce();
+
+
     }
 
     // Helper methods go here
+    private void dfs(int v) {
+        for (int w : maze.adj(v)) {
+
+            if (cycleFound) {
+                return;
+            }
+
+            if (!marked[w]) {
+                marked[w] = true;
+                cameFrom[w] = v;
+                dfs(w);
+            } else if (w != cameFrom[v]) { // When `w` is not the parent of `v` (circle found)
+                cameFrom[w] = v;
+
+                int cur = v;    // Reconstruct circle
+                edgeTo[cur] = cameFrom[cur];
+                while (cur != w) {
+                    cur = cameFrom[cur];
+                    edgeTo[cur] = cameFrom[cur];
+                }
+
+                cycleFound = true;
+                return;
+            }
+        }
+    }
 }
 
